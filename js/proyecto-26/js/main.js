4ec7391e1b64f095b02dae345440b2e87660dfb9
Adrian Peñalver <adrian1414@hotmail.es>
2024-08-21 11:15:39 +0200

    proyecto 26 JS

---
+++ js/proyecto-26/js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", event => {
+
+    let grayStar = document.querySelector(".main__star--gray");
+    let yellowStar = document.querySelector(".main__star--yellow");
+
+    grayStar.addEventListener("click", () => {
+        yellowStar.classList.add("visible");
+        grayStar.classList.add("yellow-color");
+    });
+
+    yellowStar.addEventListener("click", () => {
+        yellowStar.classList.remove("visible");
+        grayStar.classList.remove("yellow-color");
+    });
+
+});
