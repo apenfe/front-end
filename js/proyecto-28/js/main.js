91d69dbbcc9a742987e10c33bcfebff73deb99f7
Adrian Peñalver <adrian1414@hotmail.es>
2024-08-22 12:38:39 +0200

    proyecto 28 JS

---
+++ js/proyecto-28/js/main.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    // seleccionar elementos del DOM
+    let number = document.querySelector(".load__number");
+    let btn = document.querySelector(".load__btn");
+    let complete = document.querySelector(".load__complete");
+    let background = document.querySelector(".main__background");
+
+    // contadores
+    let percent = 1;
+    let blur = 30;
+
+    btn.addEventListener("click", () => {
+
+        btn.style.display = "none";
+
+        let inter = setInterval(() => {
+
+            percent++;
+            blur -= 30/100;
+
+            if(percent>100){
+                clearInterval(inter);
+                complete.style.display = "block";
+            }else{
+                number.innerHTML = percent +"%";
+                background.style.filter = `blur(${blur}px)`;
+            }
+
+        }, 20);
+
+    });
+
+});
