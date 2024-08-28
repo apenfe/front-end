110215f03857107ca1bbbecd63aca5a2437816c4
Adrian Peñalver <adrian1414@hotmail.es>
2024-08-28 10:21:22 +0200

    proyecto 38 JS

---
+++ js/proyecto-38/js/main.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    let audios = document.querySelectorAll(".audios__item");
+    let btns = document.querySelectorAll(".sounds__btn");
+
+    let start = () => {
+
+        btns.forEach((btn) => {
+
+            btn.addEventListener("click", () => {
+
+                let id = btn.getAttribute("data-audio");
+                let audio = document.querySelector("#" + id);
+
+                stop();
+
+                audio.play();
+                audio.volume = 0.3;
+
+            });
+
+        });
+
+    }
+
+    let stop = () => {
+
+        audios.forEach(audio => {
+            audio.pause();
+            audio.currentTime = 0;
+        });
+
+    }
+
+    start();
+
+});
